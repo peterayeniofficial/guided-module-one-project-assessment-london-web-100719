8be507f6229a1731a56f588f8718abd6b5c61594
Peter <ayenicology@gmail.com>
2019-10-23 09:32:04 +0100

    Finish user can create account

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -10,4 +10,21 @@ class User < ActiveRecord::Base
   # def self.latest_user
   #   User.all.last
   # end
+
+  def self.create
+    User.find_or_create_by(first_name: first_name, last_name: last_name, email: email)
+  end
+
+  def find_user_by_email(email)
+    self.find_by(email: email)
+  end
+
+  def self.delete_a_user
+    find_user_by_email.delete
+  end
+
+  def self.update_user(email, first_name, last_name)
+    user = find_user_by_email
+    user.update(first_name: first_name, last_name: last_name, email: email)
+  end
 end
